ad50016d7d3465c04689e9167ed4ed94a0e33448
dpsgy <mdamufid@gmail.com>
2023-09-12 23:03:15 +0700

    menambahkan fungsi button dan logic berhitung

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 console.log('test')
 
+function ht_luas(){
+    var s = document.getElementById('s').value;
+
+    var luas = s*s;
+
+    document.getElementById('Luas').innerHTML = luas
+}
+
+function ht_keliling(){
+    var s = document.getElementById('s').value;
+
+    var keliling = 4*s;
+
+    document.getElementById('Keliling').innerHTML = keliling
+}
